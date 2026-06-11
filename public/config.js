6dfdd4aacb87516d81ab833cc8ea304fef0bd27d
yacamoto255-ctrl <yacamoto255@gmail.com>
2026-06-11 18:57:59 -0300

    Set Railway API URL in config.js for production deploy

--- public/config.js
+++ public/config.js
@@ -4,4 +4,4 @@
 // NETLIFY + RAILWAY: coloque a URL do seu backend no Railway
 //   Ex: window.API_BASE = 'https://barberpro-api-production.up.railway.app';
 //
-window.API_BASE = '';
+window.API_BASE = 'https://barberpro-api-production.up.railway.app';
